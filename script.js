b8464215e593be61a89e43ab1de4c34e5575d1dd
Johan <jpettersen50@gmail.com>
2022-07-29 05:28:18 +0200

    add beginning of game html add gameboard & player obj

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-const game = (() => {
+const gameModule = (() => {
+'use strict';
+
+let gameArray = [];
 
 
+
+return {
+    getGameBoard: function() {
+
+    },
+
+    playLoc: function(symbol, playedLoc) {
+        gameArray[playedLoc] = symbol;
+        console.log(gameArray);
+    }
+
+};
 
     
-})
+})();
+
+
+const player = (name, symbol) => {
+
+    const getName = () => name;
+    const getSymbol = () => symbol;
+
+    return {getName, getSymbol};
+};
+
+const player1 = player('Player 1', 'X');
+const player2 = player('Player 2', 'O');
+
+gameModule.playLoc(player1.getSymbol(), 0);
