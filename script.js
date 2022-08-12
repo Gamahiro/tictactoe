d0e9c1173d37728f75856975786e0b1d70ca8b07
Johan <jpettersen50@gmail.com>
2022-08-12 18:48:02 +0200

    ye

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 const gameBox = document.querySelector('#gameBox');
 
+
 const gameModule = (() => {
 'use strict';
 
 let gameArray = [];
 
-function initGame() {
-    for(let i = 0; i < 9; i++) {
-        gameArray.push('blank');
-    }
-}
-
-
 return {
-    getGameBoard: function() {
-
-    },
 
     playLoc: function(symbol, playedLoc) {
         gameArray[playedLoc] = symbol;
         console.log(gameArray);
     },
 
-    initGame 
+    initGame: function() {
+        for(let i = 0; i < 9; i++) {
+            gameArray.push(' ');
+        }
+    },
+
+    updateBoard: function() {
+        for(let i = 0; i < gameArray.length; i++){
+            const playBox = document.createElement('div');
+            playBox.className = 'playBox';
+            playBox.innerHTML =  '' + gameArray[i];
+            gameBox.appendChild(playBox);
+            console.log(gameArray[i]);
+        }
+       
+    },
+
+    clearBoard: function() {
+        for (let i = 0; i < gameArray.length; i++) {
+            gameBox.removeChild(gameBox.lastChild);
+        }
+    }
 };
 
     
@@ -41,7 +53,13 @@ const player1 = player('Player 1', 'X');
 const player2 = player('Player 2', 'O');
 
 gameModule.initGame();
-
 gameModule.playLoc(player1.getSymbol(), 5);
 gameModule.playLoc(player2.getSymbol(), 3);
-
+gameModule.updateBoard();
+gameModule.clearBoard();
+gameModule.playLoc(player1.getSymbol(), 1);
+gameModule.playLoc(player2.getSymbol(), 4);
+gameModule.updateBoard();
+gameModule.playLoc(player1.getSymbol(), 6);
+gameModule.clearBoard();
+gameModule.updateBoard();
